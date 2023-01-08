7c98faa8b455fd1302278ed65f26a33178356e2b
Santiago <sagos0919@gmail.com>
2023-01-08 17:33:45 +0000

    Some minor changes on the geolocation script

--- assets/js/geolocation.js
+++ assets/js/geolocation.js
@@ -1,45 +1,49 @@
-// # PSEUDOCODE
-
 // * Create a submit event
 $('#search-form').on('submit', function (e) {
 	// use the prevent default method to stop the submit deleting the input
 	e.preventDefault();
 
 	// Register the value of the input
-	let searchInput = $('#search-input').val();
+	let searchInput = $('#search-input').val().trim();
 
+	if (searchInput === '' || searchInput === undefined) {
+		alert('Please, input a city name');
+		return;
+	}
+
+	// # API QUERIES
 	// Declare the API key
-	const APIKey = 'API-key';
+	const APIKey = '1a374aa07ec0bd02e61afc3e13dab4e3';
 
-	// Declare queryURL
+	// Declare query URL
 	const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&appid=${APIKey}`;
 
 	// * Use the AJAX call
 	$.ajax({
 		type: 'GET',
 		url: queryURL,
-		success: function (response) {
-			// Console Log of response Object
-			console.log('--------------');
-			console.log(response);
-			console.log('--------------');
+	}).then(function (response) {
+		// Console Log of response Object
+		console.log('--------------');
+		console.log(response);
+		console.log('--------------');
 
-			// Name of city
-			let cityName = response[0].name;
-			// Latitude
-			let latitude = response[0].lat;
-			// Longitude
-			let longitude = response[0].lon;
+		// Name of city
+		let cityName = response[0].name;
+		// Latitude
+		let latitude = response[0].lat;
+		// Longitude
+		let longitude = response[0].lon;
 
-			// Save into the local storage
-			localStorage.setItem('city', cityName);
-			localStorage.setItem('latitude', latitude);
-			localStorage.setItem('longitude', longitude);
+		// Save into the local storage
+		localStorage.setItem('city', cityName);
+		localStorage.setItem('latitude', latitude);
+		localStorage.setItem('longitude', longitude);
 
-			// * Keep the name in the input
-			let cityInput = $('#search-input').val(
-				localStorage.getItem('city')
-			);
-		},
+		// * Keep the name in the input
+		let cityInput = $('#search-input').val(localStorage.getItem('city'));
+
+		console.log(latitude, longitude);
+		initForecast(latitude, longitude);
 	});
 });
